37c34e5e6c9e93b237d22a22578dbcc89cb4855e
OAK <multioakful@gmail.com>
2014-06-01 15:16:50 +0200

    init

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,9 @@
 module UsersHelper
 
   # Returns the Gravatar (http://gravatar.com/) for the given user. Can change. Should change! 
-   def gravatar_for(user)
+   def gravatar_for(user, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}" # use this image rather 
     																#:http://www.adinkra.org/images/nyameohene_lg.gif
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
